0995014bdc9eb46fd8b6d6399747aea05a7f1ac8
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2026-07-07 15:38:15 +0200

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        if (!nums.length) {
+            return 0;
+        }
+
+        const numSet = new Set(nums);
+        let maxCount = 0;
+
+        for (const num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let i = 1;
+                while (numSet.has(num + i)) {
+                    i++;
+                }
+
+                maxCount = Math.max(maxCount, i);
+            }
+        }
+
+        return maxCount;
+    }
+}
